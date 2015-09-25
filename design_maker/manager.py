0f92f593548bd28c4f9af361d99b8fe14f952811
Nathaniel Schaaf <nathaniel.schaaf@gmail.com>
2015-09-25 10:11:45 -0500

    Make turtle only be initialized once

--- design_maker/manager.py
+++ design_maker/manager.py
@@ -8,13 +8,13 @@ class Manager(object):
     def __new__(cls):
         if Manager.__instance is None:
             Manager.__instance = super(Manager, cls).__new__(cls)
+            turtle.setup(width=1024, height=1024, startx=16, starty=16)
+            Manager.__instance.screen = turtle.Screen()
+            Manager.__instance.screen.delay(0)
+            Manager.__instance.pen = turtle.Turtle()
+            Manager.__instance.pen.speed(0)
+            Manager.__instance.pen.hideturtle()
         return Manager.__instance
 
     def __init__(self):
-        turtle.setup(width=1024, height=1024, startx=16, starty=16)
-        self.screen = turtle.Screen()
-        self.screen.delay(0)
-        self.pen = turtle.Turtle()
-        self.pen.speed(0)
-        self.pen.hideturtle()
-        
+        pass
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -4,7 +4,6 @@ from unittest import mock
 import turtle
 
 mock_turtle = mock.MagicMock()
-mock_turtle.count = 0
 mock_turtle.setup = mock.create_autospec(turtle.setup)
 mock_turtle.Screen = mock.MagicMock()
 mock_turtle.Turtle = mock.MagicMock()
@@ -14,13 +13,11 @@ from design_maker import manager
 class ManagerTest(unittest.TestCase):
 
     def setUp(self):
-        global turtle
-        self._turtle = turtle
-        turtle = mock_turtle
+        self._turtle = manager.turtle
+        manager.turtle = mock_turtle
 
     def tearDown(self):
-        global turtle
-        turtle = self._turtle
+        manager.turtle = self._turtle
 
     def test_singleton(self):
         m1 = manager.Manager()
@@ -30,7 +27,7 @@ class ManagerTest(unittest.TestCase):
     def test_manager_initializes_turtle_once(self):
         m1 = manager.Manager()
         m2 = manager.Manager()
-        self.assertEqual(1, mock_turtle.count)
+        self.assertEqual(1, mock_turtle.setup.call_count)
 
     def test_state_preservation(self):
         m = manager.Manager()
